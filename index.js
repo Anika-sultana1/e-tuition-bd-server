a2533ee81b4a959bf4c2794d6b36d6599726ab51
Anika-sultana1 <sultanaanika437@gmail.com>
2025-12-07 23:28:42 +0600

    added some features

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const app = express();
+const admin = require("firebase-admin");
 const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 
+
+
+
+
+const serviceAccount = require("./e-tuition-bd-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 app.use(cors())
 app.use(express.json())
 
-// tuitions-db
-// TktvuxLD589PjxrZ
+const verifyFirebaseToken = async(req, res, next)=>{
+
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({message:'Unauthorized access'})
+    }
+
+    const token = authorization.split(' ')[1]
+const decoded = await admin.auth().verifyIdToken(token)
+req.decoded_email = decoded.email
+
+
+next()
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sc7dsau.mongodb.net/?appName=Cluster0`;
 // const uri = `mongodb+srv://tuitions-db:TktvuxLD589PjxrZ@cluster0.sc7dsau.mongodb.net/?appName=Cluster0`;
@@ -49,30 +73,64 @@ app.post('/users',async(req, res)=>{
     res.send(result)
 })
 
-app.get('/users', async(req, res)=>{
+app.get('/users', verifyFirebaseToken, async(req, res)=>{
     const result =await usersCollections.find().toArray();
     res.send(result)
 })
 
+app.get('/users/:email/role',verifyFirebaseToken,async (req, res)=>{
+    const email = req.params.email;
+    const query = {email}
+const user = await usersCollections.findOne(query)
+res.send({role:user?.role})
+})
 
 // tuition related apis 
+app.post('/tuitions',async(req,res)=>{
+    const tuitions = req.body;
+    tuitions.status= 'pending'
+    tuitions.createdAt= new Date();
+    const result = await tuitionsCollections.insertOne(tuitions)
+    res.send(result)
+})
+
 app.get('/tuitions', async (req, res)=>{
-const {limit} =  req.query
+const {limit,email} =  req.query
+console.log('hello', email)
+
+if(email){
+    const result = await tuitionsCollections.find({email:email}).sort({createdAt:-1}).toArray();
+    console.log('result', result)
+    return  res.send(result)
+}
 
 if(!limit){
     const page = parseInt(req.query.page) || 1
     const limit = 6;
     const skip = (page-1)*limit
-    const total = await tutorCollections.countDocuments();
+    const total = await tuitionsCollections.countDocuments();
 
     const result = await tuitionsCollections.find().skip(skip).limit(limit).toArray();
-    res.send({tuitions:result, page, totalPages:Math.ceil(total/limit)})
+    return  res.send({tuitions:result, page, totalPages:Math.ceil(total/limit)})
 }
 
+    if(!email && limit){
+const result = await tuitionsCollections.find().sort({date:-1}).limit(8).toArray()
+     res.send(result)
+    }
     
-    const result = await tuitionsCollections.find().sort({date:-1}).limit(8).toArray()
-    res.send(result)
 })
+
+// app.get('/tuitions', async(req, res)=>{
+//     const email = req.query.email;
+//     const query = {}
+//     if(email){
+//         query.email = email;
+       
+//     }
+//      const result = await tuitionsCollections.find(query).sort({createdAt:-1}).toArray();
+//      res.send(result)
+// })
 
 app.get('/tutors', async(req, res)=>{
     
